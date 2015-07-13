1dbb5ea49ac10672a24371ac757a79bfaf4b2ec6
HoangVanTrinh92 <hoang.van.trinh@framgia.com>
2015-07-13 10:33:51 +0700

    fix-search-address-function

--- app/assets/javascripts/googlemapapi.js
+++ app/assets/javascripts/googlemapapi.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#mylocation').click(function(){
+  $("#mylocation").click(function(){
     geoLocation();
   })
 })
@@ -11,16 +11,15 @@ var infowindow = new google.maps.InfoWindow({
 function initialize() {
   var myLatlng = new google.maps.LatLng(21.017030, 105.783902);
   var image = "/assets/pin.png";
+  var markers = [];
   var myOptions = {
     zoom: 13,
     center: myLatlng,
     mapTypeControl: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
-
   map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-
-  var marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     position: myLatlng,
     map: map,
     icon: image,
@@ -31,27 +30,35 @@ function initialize() {
   formlng = document.getElementById("form-lon").value = myLatlng.lng();
 
   var defaultBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(20.919650305632143, 106.63326752187459));
+    new google.maps.LatLng(-33.8902, 151.1759),
+    new google.maps.LatLng(21.017030, 105.783902));
   map.fitBounds(defaultBounds);
 
-  var input = (
-    document.getElementById("pac-input"));
+  var input = (document.getElementById("pac-input"));
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   var searchBox = new google.maps.places.SearchBox((input));
   google.maps.event.addListener(searchBox, "places_changed", function() {
     var places = searchBox.getPlaces();
-
     if (places.length == 0) {
       return;
     }
     for (var i = 0, marker; marker = markers[i]; i++) {
       marker.setMap(null);
     }
+    markers = [];
     var bounds = new google.maps.LatLngBounds();
     for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(30, 30),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(13, 13)
+      };
       var marker = new google.maps.Marker({
         map: map,
+        icon: image,
         title: place.name,
         position: place.geometry.location
       });
